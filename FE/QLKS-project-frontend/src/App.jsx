28c79b71252dda09cc5b942a4c2af8fc3f0c9a76
Quoc_Anh <dohuuquocanh21dk@gmail.com>
2026-03-20 11:31:15 +0700

    Thiết kế giao diện trang Nhân viên, Báo cáo, Dịch vụ và Khách hàng

--- FE/QLKS-project-frontend/src/App.jsx
+++ FE/QLKS-project-frontend/src/App.jsx
@@ -1,25 +1,5 @@
-// import { useState } from 'react'
-// import QLkhachsan from './components/QLkhachsan'
-// import './App.css'
-// import Login from './Components/Login'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <Login></Login>
-
-//       {/* <QLkhachsan></QLkhachsan> */}
-//     </>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Components/Login";
-import Tongquan from "./components/Tongquan";
 import QLKhachSan from "./Components/QLKhachSan";
 
 function PrivateRoute({ children }) {
